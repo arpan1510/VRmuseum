405aabe8785f48e77913f21d801b65277fc322b6
Arpan Prajapati <arpanprajapati15@gmail.com>
2024-09-17 18:03:49 -0400

    Checklist for planet set up and completed earth

--- Assets/Scripts/detectPlayer.cs
+++ Assets/Scripts/detectPlayer.cs
@@ -8,16 +8,22 @@ public class detectPlayer : MonoBehaviour
     public GameObject planet;
     public GameObject proximityCircle;
     public GameObject planetUI;
+    public GameObject planetInfoSound;
     public Material material_old;
     public Material material_new;
     Renderer titleRend;
     Renderer proximityRend;
     private Animator planetRotate;
     private SphereCollider planetCollider;
+    private AudioSource planetTitleSound;
+    private AudioSource pInfoSound;
 
     void Start()
     {
         planetRotate = planet.GetComponent<Animator>();
+        planetRotate.speed = 0;
+        planetTitleSound = GetComponent<AudioSource>();
+        pInfoSound=planetInfoSound.GetComponent<AudioSource>();
         titleRend = title.GetComponent<Renderer>();
         proximityRend = proximityCircle.GetComponent<Renderer>();
         planetCollider = planet.GetComponent<SphereCollider>();
@@ -33,6 +39,7 @@ public class detectPlayer : MonoBehaviour
             planetCollider.enabled = true;
             planetRotate.speed = 1;
             planetUI.SetActive(true);
+            planetTitleSound.Play();
         }
     }
     private void OnTriggerExit(Collider other)
@@ -45,6 +52,10 @@ public class detectPlayer : MonoBehaviour
             planetCollider.enabled = false;
             planetRotate.speed = 0;
             planetUI.SetActive(false);
+        }
+        if(pInfoSound.isPlaying)
+        {
+            pInfoSound.Stop();
         }
     }
 }
