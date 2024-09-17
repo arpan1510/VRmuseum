f0a5ee69b4e08f4bdbee855c1a02c634faa1f47c
Arpan Prajapati <arpanprajapati15@gmail.com>
2024-09-17 16:08:52 -0400

    Completed till door open sound

--- Assets/Scripts/triggerDoorOpen.cs
+++ Assets/Scripts/triggerDoorOpen.cs
@@ -8,21 +8,26 @@ public class triggerDoorOpen : MonoBehaviour
     public GameObject rdoor;
     private Animator ldooranimator;
     private Animator rdooranimator;
+    private AudioSource doorSound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Markers"))
         {
             ldooranimator = ldoor.GetComponent<Animator>();
             rdooranimator = rdoor.GetComponent<Animator>();
+            doorSound=ldoor.GetComponent<AudioSource>();
             other.gameObject.SetActive(false);
             ldooranimator.SetTrigger("TrOpen");
             rdooranimator.SetTrigger("TrOpen");
+            doorSound.Play();
         }
         if(other.gameObject.name== "DoorClose")
         {
+            //doorSound.pitch = -1f;
             ldooranimator.SetTrigger("TrClose");
             rdooranimator.SetTrigger("TrClose");
             other.gameObject.SetActive(false);
+            //doorSound.Play();
         }
     }
     private void OnTriggerExit(Collider other)
